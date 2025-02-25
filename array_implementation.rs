3f6d0a09cd2584a7ac5eb95c9de02d290353c04b
asingizwe1 <asingizwe1louis1@gmail.com>
2025-02-25 20:19:13 +0300

    rust - array implementation

---
+++ array_implementation.rs
@@ -0,0 +1,13 @@
+fn main(){
+let a :[i8;10]=[42;10];
+println!("{:?}",a); //:?- for formatting arrays
+
+let primes=[2,3,5,7,11];
+for prime in primes{for i in 2..prime {
+assert_ne!(prime%i,0)
+
+}
+
+
+
+}}
