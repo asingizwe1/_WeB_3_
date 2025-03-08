1dda32726c9dc142a7c7df875e0c000f48a1621d
asingizwe1 <asingizwe1louis1@gmail.com>
2025-03-08 23:15:41 +0300

    rust - scoped spawns

---
+++ scoped_spawns.rs
@@ -0,0 +1,16 @@
+let v = vec![1, 2, 3];
+let midpoint = v.len() / 2;
+
+let handle1 = std::thread::spawn(|| {
+    let first = &v[..midpoint];
+    println!("Here's the first half of v: {first:?}");
+});
+let handle2 = std::thread::spawn(|| {
+    let second = &v[midpoint..];
+    println!("Here's the second half of v: {second:?}");
+});
+
+handle1.join().unwrap();
+handle2.join().unwrap();
+
+println!("Here's v: {v:?}");
