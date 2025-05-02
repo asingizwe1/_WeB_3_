fa624ded7b8ca7f1220dd3a4e58c5450a8314487
asingizwe1 <asingizwe1louis1@gmail.com>
2025-05-02 20:40:43 +0300

    solidity - bidirectional payment

---
+++ Bidirectional_payment.rs
@@ -0,0 +1,7 @@
+/*
+Bi-directional payment channels allow participants Alice and Bob to repeatedly transfer Ether off chain.
+Payments can go both ways, Alice pays Bob and Bob pays Alice.
+OFF CHAIN payments are cheaper than on-chain payments.
+// This is a simple implementation of a bi-directional payment channel between Alice and Bob.
+*/
+
