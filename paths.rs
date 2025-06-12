b9f01f3e72f696c68b9f548f65324caf15aa0c91
asingizwe1 <asingizwe1louis1@gmail.com>
2025-06-12 22:01:22 +0300

    Update paths.rs

--- paths.rs
+++ paths.rs
@@ -6,11 +6,13 @@ mod greetings {
 }
 fn main() {
     ////////////////absolute path
+    //absolute when you want to start from the top regardless of where you are
     ///absolute path starts from the crate root (like the main lib.rs or main.rs). It starts with crate::/
     crate::greetings::hello(); // absolute path from crate root
 
     /// ////////
     ///below is relative
+    //for relative you are in a module and younwant to go above or below
     mod outer {
         pub mod inner {
             pub fn greet() {
