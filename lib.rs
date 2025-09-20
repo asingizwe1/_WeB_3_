7d385abebc2e19bc1a9f2b52930423cdff883fa6
Dev. Louis Asingizwe <asingizwe1louis1@gmail.com>
2025-09-20 23:37:22 +0300

    if/let excercise with option<i32> type

--- lib.rs
+++ lib.rs
@@ -1,13 +1,15 @@
-pub fn sum(nums: Vec<i32>) -> i32 {
-    let v = vec![1, 2, 3];
-    let mut sum = 0;
-    //basically for i ->  the i is for each element in the vector
-    for i in v.iter() {
-        sum += i;
+pub fn unwrap_or_default(x: Option<u32>, v: u32) -> u32 {
+    if let Some(value) = x {
+        value
+    } else {
+        v
     }
 }
-
-pub fn fill(i: u32, n: usize) -> Vec<u32> {
-    //return a vector of length n filled with letter i
-    vec![i; n] //vector of length n filled with letter i
+/*pub fn unwrap_or_default(x: Option<u32>, v: u32) -> u32 {
+    todo!();
 }
+Use the if, let syntax to extract the value stored inside a Some.
+
+Return the inner value.
+
+If x is None, then return the default value v. */
