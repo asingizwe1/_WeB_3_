1e1059865d38576a3e3e6b45c25e81cfdeb8173e
Dev. Louis Asingizwe <asingizwe1louis1@gmail.com>
2025-10-05 21:44:03 +0300

    rust - error_implementation

---
+++ error_implementation.rs
@@ -0,0 +1,20 @@
+#[derive(Debug, PartialEq)]
+pub enum MathError {
+    DivByZero,
+}
+
+pub fn div(x: u32, y: u32) -> Result<u32, MathError> {
+    if y == 0 {
+        MathError::DivByZero
+    } else {
+        x / y
+    }
+}
+
+pub fn get(v: &[u32], i: usize, default_val: u32) -> u32 {
+    if i<v.len(){
+        v[i]
+    }else{
+        default_val
+    }
+}
