bea23b188cad8c427801dcef4ba9c47ac4144262
Dev. Louis Asingizwe <asingizwe1louis1@gmail.com>
2025-10-09 23:29:29 +0300

    rusts - Question operator implementation

--- lib.rs
+++ lib.rs
@@ -1,10 +1,18 @@
-pub fn parse_and_add(a: &str, b: &str) -> u32 {
-    //parsing is converting from string to number
-    let q = a.parse().expect("Failed to parse a");
-    let w = b.parse().expect("Failed to parse variable");
-    q + w
+fn parse(s: &str) -> Result<u32, String> {
+    match s.parse() {
+        Ok(val) => Ok(val),
+        Err(_) => Err("Failed to parse string into u32".to_string()),
+    }
 }
 
-pub fn unwrap_and_add(x: Option<u32>, y: Option<u32>) -> u32 {
-    x.unwrap() + y.unwrap()
+pub fn sum(nums: &[&str]) -> Result<u32, String> {
+    //parse(nums)?; -> not valid alone
+    //&[&str]->list like structure of a string of slices
+    //while parse(x) -> expects one parameter
+
+    let mut total = 0;
+    for n in nums {
+        total += parse(n)?; //if parse returns an error the function will return the error
+    }
+    Ok(total) //if all parsing is successful return total
 }
