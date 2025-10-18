a62d5a46326c93426cfc04172723deb33ea2b069
Dev. Louis Asingizwe <asingizwe1louis1@gmail.com>
2025-10-18 23:32:17 +0300

    rust - generic type implementation

--- lib.rs
+++ lib.rs
@@ -1,18 +1,15 @@
-fn parse(s: &str) -> Result<u32, String> {
-    match s.parse() {
-        Ok(val) => Ok(val),
-        Err(_) => Err("Failed to parse string into u32".to_string()),
-    }
+pub fn first<T, V>(t: (T, V)) -> T {
+    t.0
 }
 
-pub fn sum(nums: &[&str]) -> Result<u32, String> {
-    //parse(nums)?; -> not valid alone
-    //&[&str]->list like structure of a string of slices
-    //while parse(x) -> expects one parameter
+pub fn last<T, V>(t: (T, V)) -> V {
+    t.1
+}
 
-    let mut total = 0;
-    for n in nums {
-        total += parse(n)?; //if parse returns an error the function will return the error
-    }
-    Ok(total) //if all parsing is successful return total
+#[derive(Debug)]
+pub struct Rectangle<T> {
+    pub top: T,
+    pub left: T,
+    pub width: T,
+    pub height: T,
 }
